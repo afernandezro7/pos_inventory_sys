6a1cceb2ada778bbe497e60f7df71be1573c9666
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-05-09 03:45:34 -0500

    add nav sidebar-toggle buton
    delete trash buton

--- views/modules/header.module.php
+++ views/modules/header.module.php
@@ -29,5 +29,13 @@
     <!-- =============================================
     =                     NAVBAR                     =
     ============================================= -->
+    <nav class="navbar navbar-static-top">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+
+
+    </nav>
 
 </header>
