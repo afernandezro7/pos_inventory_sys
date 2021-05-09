b8ceb61248b8d9b32e9135b795335e35b6251e78
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-05-09 03:08:16 -0500

    create custom script file

--- views/default.view.php
+++ views/default.view.php
@@ -9,37 +9,40 @@
 
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="views/bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="views/bower_components/font-awesome/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="views/bower_components/Ionicons/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="views/dist/css/AdminLTE.css">
-  <!-- AdminLTE Skins. -->
-  <link rel="stylesheet" href="views/dist/css/skins/_all-skins.min.css">
+ 
+ <!-- =============================================
+ =                   CSS                   =
+ ============================================= -->
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="views/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="views/bower_components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="views/bower_components/Ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="views/dist/css/AdminLTE.css">
+    <!-- AdminLTE Skins. -->
+    <link rel="stylesheet" href="views/dist/css/skins/_all-skins.min.css">
+ <!-- ============  End of CSS  ============= -->
 
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
-  <!-- jQuery 3 -->
-  <script src="views/bower_components/jquery/dist/jquery.min.js"></script>
-  <!-- Bootstrap 3.3.7 -->
-  <script src="views/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-  <!-- SlimScroll -->
-  <script src="views/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-  <!-- FastClick -->
-  <script src="views/bower_components/fastclick/lib/fastclick.js"></script>
-  <!-- AdminLTE App -->
-  <script src="views/dist/js/adminlte.min.js"></script>
-  <!-- AdminLTE for demo purposes -->
-  <script src="views/dist/js/demo.js"></script>
-  <script>
-    $(document).ready(function () {
-      $('.sidebar-menu').tree()
-    })
-  </script>
+  <!-- =============================================
+  =                   scripts                   =
+  ============================================= -->
+    <!-- jQuery 3 -->
+    <script src="views/bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap 3.3.7 -->
+    <script src="views/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>    
+    <!-- FastClick -->
+    <script src="views/bower_components/fastclick/lib/fastclick.js"></script>
+    <!-- AdminLTE App -->
+    <script src="views/dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="views/dist/js/demo.js"></script>
+    
+  <!-- ============  End of scripts  ============= -->
 </head>
 
 <body class="hold-transition skin-blue sidebar-mini">
@@ -657,6 +660,6 @@
 </div>
 <!-- ./wrapper -->
 
-
+<script src="views/js/custom.js"></script>
 </body>
 </html>
