40ecd7bd7bafc01dd38e93a27a0ef4341acd6d0b
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-05-09 03:40:20 -0500

    add logo

---
+++ views/modules/header.module.php
@@ -0,0 +1,33 @@
+<header class="main-header">
+
+    <!-- =============================================
+    =                     LOGO                       =
+    ============================================= -->
+    <a href="#" class="logo">
+        
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini">
+            <img 
+                src="views/img/template/icono-blanco.png" 
+                alt="logo mini"
+                class="img-responsive"
+                style="padding:10px;"
+            >
+      </span>
+
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg">
+            <img 
+                src="views/img/template/logo-blanco-lineal.png" 
+                alt="logo mini"
+                class="img-responsive"
+                style="padding:10px 0px;"
+            >
+      </span>
+    </a>
+
+    <!-- =============================================
+    =                     NAVBAR                     =
+    ============================================= -->
+
+</header>
