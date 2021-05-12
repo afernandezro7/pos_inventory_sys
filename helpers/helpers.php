2847a358b0dc40c852a09f4deaf0db46205b0bf4
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-05-12 17:38:53 -0500

    update plugin timelong and change in php.ini "America/Panama" time zone

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -2,61 +2,72 @@
 
 class Helpers{
 
-	public static function LongTimeFilter($date) {
+	static public function LongTimeFilter($date) {
         if ($date == null) {
             return "Sin fecha";
         }
- 
-        $start_date = $date;
-        $since_start = $start_date->diff(new DateTime(date("Y-m-d") . " " . date("H:i:s")));
- 
-        if ($since_start->y == 0) {
-            if ($since_start->m == 0) {
-                if ($since_start->d == 0) {
-                    if ($since_start->h == 0) {
-                        if ($since_start->i == 0) {
-                            if ($since_start->s == 0) {
-                                $result = $since_start->s . ' segundos';
+
+		$format = "y-m-d H:i:s";
+		$now = date ($format);
+		$nowArr = date_parse($now);
+		$lastLogArr = date_parse($date);
+
+		$since_start = array(
+			"y"=>$nowArr['year'] - $lastLogArr['year'],
+			"m"=>$nowArr['month'] - $lastLogArr['month'],
+			"d"=>$nowArr['day'] - $lastLogArr['day'],
+			"h"=>$nowArr['hour'] - $lastLogArr['hour'],
+			"i"=>$nowArr['minute'] - $lastLogArr['minute'],
+			"s"=>$nowArr['second'] - $lastLogArr['second']
+		);
+
+        if ($since_start['y'] == 0) {
+            if ($since_start['m'] == 0) {
+                if ($since_start['d'] == 0) {
+                    if ($since_start['h'] == 0) {
+                        if ($since_start['i'] == 0) {
+                            if ($since_start['s'] == 0) {
+                                $result = $since_start['s'] . ' segundos';
                             } else {
-                                if ($since_start->s == 1) {
-                                    $result = $since_start->s . ' segundo';
+                                if ($since_start['s'] == 1) {
+                                    $result = $since_start['s'] . ' segundo';
                                 } else {
-                                    $result = $since_start->s . ' segundos';
+                                    $result = $since_start['s'] . ' segundos';
                                 }
                             }
                         } else {
-                            if ($since_start->i == 1) {
-                                $result = $since_start->i . ' minuto';
+                            if ($since_start['i'] == 1) {
+                                $result = $since_start['i'] . ' minuto';
                             } else {
-                                $result = $since_start->i . ' minutos';
+                                $result = $since_start['i'] . ' minutos';
                             }
                         }
                     } else {
-                        if ($since_start->h == 1) {
-                            $result = $since_start->h . ' hora';
+                        if ($since_start['h'] == 1) {
+                            $result = $since_start['h'] . ' hora';
                         } else {
-                            $result = $since_start->h . ' horas';
+                            $result = $since_start['h'] . ' horas';
                         }
                     }
                 } else {
-                    if ($since_start->d == 1) {
-                        $result = $since_start->d . ' día';
+                    if ($since_start['d'] == 1) {
+                        $result = $since_start['d'] . ' día';
                     } else {
-                        $result = $since_start->d . ' días';
+                        $result = $since_start['d'] . ' días';
                     }
                 }
             } else {
-                if ($since_start->m == 1) {
-                    $result = $since_start->m . ' mes';
+                if ($since_start['m'] == 1) {
+                    $result = $since_start['m'] . ' mes';
                 } else {
-                    $result = $since_start->m . ' meses';
+                    $result = $since_start['m'] . ' meses';
                 }
             }
         } else {
-            if ($since_start->y == 1) {
-                $result = $since_start->y . ' año';
+            if ($since_start['y'] == 1) {
+                $result = $since_start['y'] . ' año';
             } else {
-                $result = $since_start->y . ' años';
+                $result = $since_start['y'] . ' años';
             }
         }
  
@@ -65,3 +76,4 @@ class Helpers{
 
 
 }
+
