6196f3c482fcb30a501c265fa2cb1de4d6e7efdb
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-05-14 18:13:11 -0500

    create get Permision and process image

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,25 +1,27 @@
 <?php
 
-class Helpers{
+class Helpers
+{
 
-	static public function LongTimeFilter($date) {
+    static public function LongTimeFilter($date)
+    {
         if ($date == null) {
             return "Sin fecha";
         }
 
-		$format = "y-m-d H:i:s";
-		$now = date ($format);
-		$nowArr = date_parse($now);
-		$lastLogArr = date_parse($date);
+        $format = "y-m-d H:i:s";
+        $now = date($format);
+        $nowArr = date_parse($now);
+        $lastLogArr = date_parse($date);
 
-		$since_start = array(
-			"y"=>$nowArr['year'] - $lastLogArr['year'],
-			"m"=>$nowArr['month'] - $lastLogArr['month'],
-			"d"=>$nowArr['day'] - $lastLogArr['day'],
-			"h"=>$nowArr['hour'] - $lastLogArr['hour'],
-			"i"=>$nowArr['minute'] - $lastLogArr['minute'],
-			"s"=>$nowArr['second'] - $lastLogArr['second']
-		);
+        $since_start = array(
+            "y" => $nowArr['year'] - $lastLogArr['year'],
+            "m" => $nowArr['month'] - $lastLogArr['month'],
+            "d" => $nowArr['day'] - $lastLogArr['day'],
+            "h" => $nowArr['hour'] - $lastLogArr['hour'],
+            "i" => $nowArr['minute'] - $lastLogArr['minute'],
+            "s" => $nowArr['second'] - $lastLogArr['second']
+        );
 
         if ($since_start['y'] == 0) {
             if ($since_start['m'] == 0) {
@@ -70,10 +72,46 @@ class Helpers{
                 $result = $since_start['y'] . ' años';
             }
         }
- 
+
         return "Hace " . $result;
     }
 
+    static public function processImage($file, $dir, $name)
+    {
+        //cut image to 500x500
+        list($width, $height) = getimagesize($file['tmp_name']);
+        $newWidth = 500;
+        $newHeight = 500;
 
+        $path = "";
+
+        if ($file['type'] == "image/jpeg") {
+            $path = $dir . $name . ".jpg";
+
+            $origin = imagecreatefromjpeg($file['tmp_name']);
+            $destiny = imagecreatetruecolor($newWidth, $newHeight);
+
+            imagecopyresized($destiny, $origin, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+            imagejpeg($destiny, $path);
+        }
+
+        if ($file['type'] == "image/png") {
+            $path =  $dir . $name . ".png";
+
+            $origin = imagecreatefrompng($file['tmp_name']);
+            $destiny = imagecreatetruecolor($newWidth, $newHeight);
+
+            imagecopyresized($destiny, $origin, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+            imagepng($destiny, $path);
+        }
+
+        return $path;
+    }
+
+    static public function getPermission($role,Array $rolesAdmited)
+    {
+        return in_array ( $role , $rolesAdmited );
+    }
 }
-
