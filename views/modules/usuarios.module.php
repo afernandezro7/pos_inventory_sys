1bc79d5a05a3a06bf7dabf8a8f3b0cd3ab96f220
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-05-15 17:03:21 -0500

    Change user Module title

--- views/modules/usuarios.module.php
+++ views/modules/usuarios.module.php
@@ -2,8 +2,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
-            Usuarios
-            <small>Panel de Control</small>
+        Administrar Usuarios
         </h1>
         <ol class="breadcrumb">
             <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
