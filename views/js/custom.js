b8f61d3501bc4ee15e190074f9229032e3cbb1e9
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-05-16 00:38:56 -0500

    add icheck plugin and activate it

--- views/js/custom.js
+++ views/js/custom.js
@@ -34,3 +34,11 @@ $('.customTables').DataTable({
 		}
     }
 });
+
+/*=============================================
+=    iCHECK for checkbox and radio inputs     =
+=============================================*/
+$('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+	checkboxClass: 'icheckbox_minimal-blue',
+	radioClass   : 'iradio_minimal-blue'
+})
