ddd9d39352292248e9ff617de842713c284b662b
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-05-15 19:09:14 -0500

    add active to links on sidebar

--- views/modules/sidebar.module.php
+++ views/modules/sidebar.module.php
@@ -1,3 +1,6 @@
+<?php 
+    $route = $_GET['ruta'];
+?>
 <aside class="main-sidebar">
 
     <section class="sidebar">
@@ -15,37 +18,37 @@
 
         <!-- sidebar menu -->
         <ul class="sidebar-menu" data-widget="tree">
-            <li class="active">
+            <li class="<?=($route=="inicio") ? 'active' : ''?>">
                 <a href="inicio">
                     <i class="fa fa-home"></i>
                     <span>Inicio</span>
                 </a>
             </li>
-            <li>
+            <li class="<?=($route=="usuarios") ? 'active' : ''?>">
                 <a href="usuarios">
                     <i class="fa fa-user"></i>
                     <span>Usuarios</span>
                 </a>
             </li>
-            <li>
+            <li class="<?=($route=="categorias") ? 'active' : ''?>">
                 <a href="categorias">
                     <i class="fa fa-th"></i>
                     <span>Categorías</span>
                 </a>
             </li>
-            <li>
+            <li class="<?=($route=="productos") ? 'active' : ''?>">
                 <a href="productos">
                     <i class="fa fa-product-hunt"></i>
                     <span>Productos</span>
                 </a>
             </li>
-            <li>
+            <li class="<?=($route=="clientes") ? 'active' : ''?>">
                 <a href="clientes">
                     <i class="fa fa-users"></i>
                     <span>Clientes</span>
                 </a>
             </li>
-            <li class="treeview">
+            <li class="treeview <?=($route=="ventas" || $route=="crear-venta" || $route=="reportes") ? 'active' : ''?>">
                 <a href="">
                     <i class="fa fa-list-ul"></i>
                     <span>Ventas</span>
@@ -54,19 +57,19 @@
                     </span>
                 </a>
                 <ul class="treeview-menu">
-                    <li>
+                    <li class="<?=($route=="ventas") ? 'active' : ''?>">
                         <a href="ventas">
                             <i class="fa fa-circle-o"></i>
                             <span>Administrar Ventas</span>
                         </a>
                     </li>
-                    <li>
+                    <li class="<?=($route=="crear-venta") ? 'active' : ''?>">
                         <a href="crear-venta">
                             <i class="fa fa-circle-o"></i>
                             <span>Crear Venta</span>
                         </a>
                     </li>
-                    <li>
+                    <li class="<?=($route=="reportes") ? 'active' : ''?>">
                         <a href="reportes">
                             <i class="fa fa-circle-o"></i>
                             <span>Reporte de Ventas</span>
