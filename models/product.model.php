5cda886622c055f290ceacbc2fed74958fcc6b95
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-05-18 15:38:14 -0500

    add create product in model

--- models/product.model.php
+++ models/product.model.php
@@ -18,8 +18,38 @@ class Product{
                 LIMIT 1";
         $stmt = Connection::connect()->prepare($sql);
         $stmt -> bindParam(':idCategory', $idCategory, PDO::PARAM_STR);
-        $response = $stmt->execute();
+        $stmt->execute();
         return $stmt->fetch();
 
     }
+
+    static public function createProduct(Array $data)
+    {
+        $sql = "INSERT INTO products(category_id, barcode, description, image, stock, cost, sell_price) VALUES (:category_id, :barcode, :description, :image, :stock, :cost, :sell_price)";
+        $stmt = Connection::connect()->prepare($sql);
+        $stmt -> bindParam(':category_id', $data['category_id'], PDO::PARAM_STR);
+        $stmt -> bindParam(':barcode', $data['barcode'], PDO::PARAM_STR);
+        $stmt -> bindParam(':description', $data['description'], PDO::PARAM_STR);
+        $stmt -> bindParam(':image', $data['image'], PDO::PARAM_STR);
+        $stmt -> bindParam(':stock', $data['stock'], PDO::PARAM_STR);
+        $stmt -> bindParam(':cost', $data['cost'], PDO::PARAM_STR);
+        $stmt -> bindParam(':sell_price', $data['sell_price'], PDO::PARAM_STR);
+        $response = $stmt->execute();
+
+        if($response){
+            return array(
+                'ok'=>true,
+                'type'=>'success',
+                'msg'=>'Producto creado correctamente'
+            );
+        }else{
+            return array(
+                'ok'=>false,
+                'type'=>'error',
+                'msg'=>'Error creando producto contacte soporte'
+            );
+
+        } 
+    }
+
 }
