ea03678e68f6748e29d93ab1cd54c380439fff1b
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-05-19 15:34:59 -0500

    add ctrEditClient

--- controllers/clients.controller.php
+++ controllers/clients.controller.php
@@ -70,5 +70,95 @@ class ClientsController{
 		}
 	}
 
+	static public function ctrEditClient(){
+		/*POST */
+	// editClientId
+	// editClientName
+    // editClientIdentity
+    // editClientEmail
+    // editClientPhone
+    // editClientAddress
+
+    // editClientBirthDate
+	if(isset($_POST['editClientId'])){
+
+		$clientId = $_POST['editClientId'];
+		$clientDb= Product::findOne("id",$clientId);
+		$editClient = $clientDb;
+
+		//verify if product exists
+		if($clientDb){
+
+			// EDIT name
+			if(isset($_POST['editClientName']) && 
+			  !empty($_POST['editClientName']) && 
+			   preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚü ]+$/', $_POST['editClientName']))
+			{
+				$editClient['name'] = $_POST['editClientName'];
+			}
+
+			// EDIT identity
+			if(isset($_POST['editClientIdentity']) && !empty($_POST['editClientIdentity']))
+			{
+				$editClient['identity'] = $_POST['editClientIdentity'];
+			}
+			
+			// EDIT email
+			if(isset($_POST['editClientEmail']) && 
+			!empty($_POST['editClientEmail']) && 
+			filter_var($_POST['editClientEmail'], FILTER_VALIDATE_EMAIL))
+			{
+				$editClient['email'] = $_POST['editClientEmail'];
+			}
+			
+			// EDIT phone
+			if(isset($_POST['editClientPhone']) && !empty($_POST['editClientPhone']))
+			{
+				$editClient['phone'] = $_POST['editClientPhone'];
+			}
+
+			// EDIT address
+			if(isset($_POST['editClientAddress']) && !empty($_POST['editClientAddress']))
+			{
+				$editClient['address'] = $_POST['editClientAddress'];
+			}
+
+			// EDIT birth
+			if(isset($_POST['editClientBirthDate']) && !empty($_POST['editClientBirthDate']))
+			{
+				$editClient['address'] = $_POST['editClientBirthDate'];
+			}
+		
+			$response = Client::editClient($editClient);
+			$type = $response['type'];
+			$msg = $response['msg'];
+
+			echo "<script>
+				swal({
+					type: '".$type."',
+					title: '".$msg."',
+					closeOnConfirm: false
+				 }).then((res)=>{
+					if(res.value){
+						window.location = 'clientes';
+					}
+				});
+			</script>";
+
+		}else{
+			echo "<script>
+				swal({
+					type: 'error',
+					title: 'Cliente no encontrado, error al editarlo'
+				 }).then((res)=>{
+					if(res.value){
+						window.location = 'clientes';
+					}
+				});
+			</script>";
+		}
+	}
+}
+
 
 }
