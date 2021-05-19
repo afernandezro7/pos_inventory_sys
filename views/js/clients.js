179b2e1fa39c6a505102aa8534bf95f228b9bd08
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-05-19 18:22:46 -0500

    swal modal to comfirm delete

--- views/js/clients.js
+++ views/js/clients.js
@@ -1,5 +1,5 @@
 /*=============================================
-=                  EDIT PRODUCT               =
+=                  EDIT Client               =
 =============================================*/
 
 $(document).on('click',".btnEditClient",function(){
@@ -40,4 +40,27 @@ $(document).on('click',".btnEditClient",function(){
             }
         }
     })
+})
+
+/*=============================================
+=               DELETE Client                =
+=============================================*/
+$(document).on('click',".btn_delete_client",function(){
+    var btn = $(this)
+    var idClient = $(this).attr("idClient");
+
+    swal({
+        type: 'warning',
+        title: '¿Está seguro de borrar el cliente?',
+        text: "¡Si no lo está puede cancelar la acción",
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'Cancelar',
+        comfirmButtonText: 'Si, borrar cliente!',
+    }).then((res)=>{
+        if(res.value){
+            window.location = 'index.php?ruta=clientes&idTodelete='+idClient;
+        }
+    });
 })
