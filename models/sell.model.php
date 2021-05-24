ef6fbeee032c1e858ded0bbdb99b1c5814ff458a
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-05-24 18:40:45 -0500

    simple query for all sells on model

--- models/sell.model.php
+++ models/sell.model.php
@@ -10,6 +10,16 @@ class Sell{
 
     }
 
+    static public function findAllBasic(){
+
+        $sql = "SELECT s.*, u.name AS 'vendor',c.name AS 'client' FROM sells s INNER JOIN clients c ON s.client_id= c.id INNER JOIN users u ON s.vendor_id= u.id ORDER BY s.createdAt ASC";
+        $stmt = Connection::connect()->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+
+    }
+
 
 
 }
